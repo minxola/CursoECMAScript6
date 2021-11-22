43284c6d8af3e84ec5614a70d3090d31ef282aea
minxola <reminxola69@gmail.com>
2021-11-22 08:03:43 -0500

    First commit local

---
+++ ECMA Script 6+.mm
@@ -0,0 +1,114 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="ECMA Script 6+" LOCALIZED_STYLE_REF="styles.topic" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1635031512933" COLOR="#660066">
+<font SIZE="12"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/standard-1.6.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Bienvenida al curso" POSITION="right" ID="ID_1198007814" CREATED="1635031557918" MODIFIED="1635031567238">
+<edge COLOR="#ff0000"/>
+<node TEXT="1 Que es ECMA Script" ID="ID_783673402" CREATED="1635031568228" MODIFIED="1635031680863">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Que se implemento en ES6" POSITION="right" ID="ID_1442190042" CREATED="1635031583008" MODIFIED="1635031591146">
+<edge COLOR="#0000ff"/>
+<node TEXT="2 Default params y concatenacion" ID="ID_353570953" CREATED="1635031592125" MODIFIED="1635031684952">
+<font BOLD="true"/>
+</node>
+<node TEXT="3 Let, Const, Multilínea, Spread Operator y Desestructuración" ID="ID_508510628" CREATED="1635031604539" MODIFIED="1635031684968">
+<font BOLD="true"/>
+</node>
+<node TEXT="4 Arrow functions, promesas y parámetros en objetos" ID="ID_653194699" CREATED="1635031639531" MODIFIED="1635031684983">
+<font BOLD="true"/>
+</node>
+<node TEXT="5 Clases, módulos y generadores" ID="ID_1148931866" CREATED="1635031663859" MODIFIED="1635031684983">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Que se implementó en ES7" POSITION="right" ID="ID_908424278" CREATED="1635031695975" MODIFIED="1635031709709">
+<edge COLOR="#00ff00"/>
+<node TEXT="6 Que se implemento en ES7" ID="ID_634154744" CREATED="1635046641967" MODIFIED="1635046810390">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Que se implementó en ES8" POSITION="right" ID="ID_1160103564" CREATED="1635046666272" MODIFIED="1635046674039">
+<edge COLOR="#ff00ff"/>
+<node TEXT="7 Que se implementó en ES8" ID="ID_86749978" CREATED="1635046675633" MODIFIED="1635046686416"/>
+<node TEXT="8 Async Await" ID="ID_1871686525" CREATED="1635046687104" MODIFIED="1635046696183"/>
+</node>
+<node TEXT="Actualidad y próximos pasos en ECMAScript" POSITION="right" ID="ID_326471582" CREATED="1635046700261" MODIFIED="1635046718330">
+<edge COLOR="#00ffff"/>
+<node TEXT="9 Que se implementó en ES9" ID="ID_1206702222" CREATED="1635046719720" MODIFIED="1635046730752"/>
+<node TEXT="10 Que se implementó en ES10" ID="ID_658909458" CREATED="1635046731049" MODIFIED="1635046743863"/>
+<node TEXT="11 ECMAScript 2020" ID="ID_214682179" CREATED="1635046745253" MODIFIED="1635046755219"/>
+<node TEXT="12 ECMAScript 2020 - Características importantes" ID="ID_1965632710" CREATED="1635046761362" MODIFIED="1635046778189"/>
+<node TEXT="13 ECMAScript 2021" ID="ID_958782643" CREATED="1635046779746" MODIFIED="1635046788605"/>
+<node TEXT="14 TC39 y cierre del curso" ID="ID_1660070468" CREATED="1635046791230" MODIFIED="1635046799775"/>
+</node>
+</node>
+</map>
